474b77dfb4cb103ea281cbf6b344e641f08fe7e5
SlvesterElormCoch <Sylvester.E.Coch.22@dartmouth.edu>
2020-03-29 14:25:48 -0500

    initial react setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require(firebase);
+require(firebase/firestore)
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB7t4vcRtPHWMtvLTC0tK9ZXSFdGBTFNJc",
+    authDomain: "evernote-clone-908d1.firebaseapp.com",
+    databaseURL: "https://evernote-clone-908d1.firebaseio.com",
+    projectId: "evernote-clone-908d1",
+    storageBucket: "evernote-clone-908d1.appspot.com",
+    messagingSenderId: "37437546919",
+    appId: "1:37437546919:web:dcc162da6ea8edfa4d5682"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
